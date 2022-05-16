56dceed2ddc3ba8abd5bb61346b22387ddf53c15
smnoh <smnoh@ui-lab.co.kr>
2022-05-16 15:35:52 +0900

    #vue setting

---
+++ apartmentsale/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="test-area">test</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
